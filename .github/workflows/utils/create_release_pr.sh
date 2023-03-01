acf9ed9239352aefe33008fc39273b32081c9330
Kosuke Numata <coursek8814@gmail.com>
2023-03-01 19:37:21 +0900

    :recycle: ベースブランチの決め打ちを修正

--- .github/workflows/utils/create_release_pr.sh
+++ .github/workflows/utils/create_release_pr.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-BODY=${git log production..main --pretty=format:'- %h : %s' --abbrev-commit --merges}
+BODY=${git log main..${BASE_BRANCH} --pretty=format:'- %h : %s' --abbrev-commit --merges}
 gh pr create --base ${BASE_BRANCH} --title "${PR_TITLE}" --body "${BODY}"
